11f054ee6c2f1f4a3a20a29276a85580340b7906
lem01 <lemcisneros01@gmail.com>
2019-09-26 20:43:44 -0600

    nuevos cambios

---
+++ header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="estilos.css">
+    <title>Vib-Lib</title>
+</head>
+<body>
+
+    <header>
+  <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+                <a style="color:#181717" class="navbar-brand login-form-font-header" href="index.html">Vip-<span>Lib</span></a>
+             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                  <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                      <a class="nav-link" href="inicio">Incio </a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Inicio 2</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Inicio 3</a>
+                    </li> 
+                    
+                  
+                    <li class="">
+                 <form class="form-inline" action="/action_page.php">
+                    <input class="form-control mr-sm-2" type="text" placeholder="Buscar Libro">
+                    <br><br>
+                    <button class="btn btn-success" type="submit">Buscar</button>
+                 </form>
+                    </li>
+
+                  </ul>
+                </div>  
+              </nav>
+              <br>
+           
+              <!-- fin header -->
+            </header>   
+
+              <div class="container">
+                <h3>Binevenidos</h3>
+                <p>ste es el inicio de la pagina</p>
+              </div>
+
+
+            
+              
+
+<script src="bootstrap/jquery.js"></script>
+<script src="bootstrap/js/bootstrap.js"></script>
+</body>
+</html>
