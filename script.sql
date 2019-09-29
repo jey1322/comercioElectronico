4e44e8d17bd455ea03f475befdbfad8d2254aa9e
jey1322 <jeypaniagua@gmail.com>
2019-09-29 13:05:57 -0600

    se agrega datos en tecnologia

--- script.sql
+++ script.sql
@@ -10,3 +10,4 @@ insert into libros values ('Bodas de sangre','5','9.99'),('Guerra y Paz','6','16
 
 insert into ropa values ('Traje de baño','15','24.99'),('Crop Top','5','14.99'),('Pantalon Hombre','12','16.99'),('Short para dama','5','9.99'),('Blusa','15','11.99'),('Esmoquin','20','64.99'),('Esmoquin crema','20','84.99'),('Vestido Casual','15','89.99'),('Vestido','12','119.99');
 
+insert into tecnologia values ('Pc Gamer','1','1999.99'),('Xiaomi mi band','20','39.99'),('i7 TWS','25','11.99'),('Laptop HP','2','299.99'),('Samsung curved','1','799.99'),('Samsung s10','5','899.99'),('Mause gamer','25','6.99'),('Teclado gamer','25','21.99'),('PS4','10','599.99');
