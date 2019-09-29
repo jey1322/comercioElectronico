71a251ed2db5a35ef255ec0bbb50d80efdad1ef4
jey1322 <jeypaniagua@gmail.com>
2019-09-29 12:56:16 -0600

    se agrega datos en libros

--- script.sql
+++ script.sql
@@ -6,4 +6,5 @@ create table ropa(nombre varchar (100) not null, cantidad int(5) not null, preci
 create table tecnologia(nombre varchar(100) not null, cantidad int(5) not null, precio int(10) not null);
 
 insert into libros values ('El nombre de la Rosa','2','14.99');
-insert into libros values ('Bodas de sangre','5','9.99'),('Guerra y Paz','6','16.99');
+insert into libros values ('Bodas de sangre','5','9.99'),('Guerra y Paz','6','16.99'),('Orgullo y prejuicio','2','19.99'),('Como agua para chocolate','5','9.99'),('El lobo estepario','5','24.99'),('El arte de amar','2','12.99'),('En busco del tiempo perdido','1','21.99'),('Ulises','10','24.99');
+
