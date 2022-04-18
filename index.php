206886463e1550c9e14ab07abbc3084d7237a64c
jey1322 <jeypaniagua@gmail.com>
2022-04-18 06:53:49 +0200

    cambios en index

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ require_once 'app/start.php';
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Vip-Lib shop</title>
+    <title>Vip-Lib Tienda propiedad de Code-in</title>
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
